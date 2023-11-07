7ce5fca1a082e72a7a485edf3fca2f855166e4f8
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-07 19:40:18 -0300

    Layout desktop

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Aside from "./components/Aside";
 import Banner from "./components/Banner";
 import banner from './assets/banner.png'
+import Galeria from "./components/Galeria";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -11,17 +12,43 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
 `;
 
+const AppContainer = styled.div`
+  width: 1320px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
 function App() {
 
   return (
     <FundoGradiente>
       <GlobalStyles />
-      <Header />
-      <Aside />
-      <Banner 
-        backgroundImage={banner}
-        texto="A galeria mais completa de fotos do espaço!"
-      />
+      <AppContainer>
+        <Header />
+        <MainContainer>
+          <Aside />
+          <ConteudoGaleria>
+            <Banner 
+              backgroundImage={banner}
+              texto="A galeria mais completa de fotos do espaço!"
+            />
+            <Galeria />
+          </ConteudoGaleria>
+          
+        </MainContainer>
+        
+      </AppContainer>
     </FundoGradiente>
   )
 }
