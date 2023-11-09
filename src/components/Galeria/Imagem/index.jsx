f4c3d4fa3804d158070a628281c04454b9addac8
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-11-09 18:55:37 -0300

    Adicionando badge nas imagens da galeria

--- src/components/Galeria/Imagem/index.jsx
+++ src/components/Galeria/Imagem/index.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components"
 import IconButton from "../../IconButton";
 
 const Card = styled.figure`
+    position: relative;
     width: ${props => props.$expandida ? '90%' : "400px"};
     max-width: 100%;
     margin: 0;
     display: flex;
     flex-direction: column;
     & > img {
+        position: relative;
         max-width: 100%;
         border-radius: 20px 20px 0 0;
     }
@@ -36,6 +38,17 @@ const Card = styled.figure`
     }
 `;
 
+const Badger = styled.div`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: rgba(153, 169, 203, 0.3);
+    color: #FFF;
+    font-size: 18px;
+    padding: 8px 8px;
+    border-radius: 8px;
+`;
+
 const Imagem = ({ foto, expandida = false, aoZoomSolicitado, aoAlternarFavorito }) => {
 
     const iconeFavorito = foto.favorita ? "/icons/favorito-ativo.png" : "/icons/favorito.png"
@@ -43,6 +56,7 @@ const Imagem = ({ foto, expandida = false, aoZoomSolicitado, aoAlternarFavorito 
     return (
         <Card $expandida={expandida} id={`foto-${foto.id}`}>
             <img src={foto.path} alt={foto.titulo} />
+            <Badger>{foto.tag}</Badger>
             <figcaption>
                 <h3>{foto.titulo}</h3>
                 <footer>
